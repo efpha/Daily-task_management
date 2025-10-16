573cc18177075edf313f64f5d81b2d39b2545e67
KEFFA <muthurikeffa254@gmail.com>
2025-10-16 20:48:52 +0300

    Logging redirect to dashboard

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-def home(request):
-    return render(request, 'pages/home.html')
+
+def home(req):
+    return render(req, 'pages/home.html')
+
+def dashboard_page(req):
+    #make sure the user is logged in
+    if 'access_token' not in req.session:
+        return redirect('login_page')
+    return render(req, 'pages/dashboard.html')
