9fa7223915e5ee77ba3472b20b76dd1393eb2769
KEFFA <muthurikeffa254@gmail.com>
2025-10-16 20:47:44 +0300

    API path update

--- users/urls.py
+++ users/urls.py
@@ -5,11 +5,7 @@ from . import views
 from .views import register_user, user_login
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('login/api/', user_login, name='login_user'),
-    path('login/test/', views.test_login_endpoint, name='test_login_endpoint'),
-    path('register/', views.register_view, name='register'),
-    path('register/api/', register_user, name='register_user'),
-    path('register/test/', views.test_register_endpoint, name='test_register_endpoint'),
+    path('login/', user_login, name='login_user'),
+    path('register/', register_user, name='register_user'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
