55c447931433d433ee415eaab3c3c4a42ac6acda
KEFFA <muthurikeffa254@gmail.com>
2025-10-16 20:49:58 +0300

    Form submission and rendering for logging and registering

--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+from http.client import responses
+
 from django.contrib.auth.hashers import check_password
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import UserProfile
@@ -114,9 +116,47 @@ def user_profile(req):
     )
 
 
-
-
 #register page rendering and form submission
-# def register_page(req):
-#     if req.method == 'POST':
-#         name = req
+def register_page(req):
+    if req.method == 'POST':
+        name = req.POST.get('name')
+        email = req.POST.get('email')
+        password = req.POST.get('password')
+
+        data = {'name': name, 'email': email, 'password': password}
+        api_url = 'http://127.0.0.1:8000/users/register/api/'
+
+        response = requests.post(api_url, json=data)
+
+        if response.status_code==201:
+            messages.success(req, 'Registration succesful')
+            return redirect('login_page')
+        else:
+            messages.error(req, 'Registration failed. Try again')
+
+    return render(req, 'users/register.html')
+
+
+#Login page rendering and form submission
+def login_page(req):
+    if req.method == 'POST':
+        email = req.POST.get('email')
+        password = req.POST.get('password')
+
+        data = {'email': email, 'password': password}
+        api_url = 'http://127.0.0.1:8000/users/login/api'
+
+        response = requests.post(api_url, json=data)
+
+        if response.status_code == 200:
+            token_data = response.json()
+            req.session['access_token'] = token_data.get('access')
+            req.session['refresh_token'] = token_data.get('refresh')
+            messages.success(req, 'Login successful')
+            return redirect('dashboard_page')
+        else:
+            messages.error(req, 'Invalid credentials')
+
+    return render(req, 'users/login.html')
+
+
