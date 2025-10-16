3f3779b0f576f681d95ea4caeeff1c51bd44bcb7
KEFFA <muthurikeffa254@gmail.com>
2025-10-17 01:03:15 +0300

    Dashboard path inclusion

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import dashboard_page
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='home'), #home when not logged in
+    path('dashboard/', views.dashboard_page, name='dashboard_page')
 ]
