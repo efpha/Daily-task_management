2d04a2fdffed2669000c7d53818134bbb566c469
KEFFA <muthurikeffa254@gmail.com>
2025-10-19 22:36:07 +0300

    Cors installation

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'users',
     'tasks',
-    'pages'
+    'pages',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'task_manager.urls'
 
